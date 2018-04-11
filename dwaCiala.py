247e6b3ad61e1c8c9d7c670fbde5ba10470a36b6
PDzikus <pawelwiszniewski@gmail.com>
2018-04-11 23:16:35 +0200

    Dzialajace równani grawitacji dla dwóch ciał

--- dwaCiala.py
+++ dwaCiala.py
@@ -7,45 +7,58 @@ Created on Mon Apr  9 21:00:07 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-masaOrbit = 10
-distance = 10000
-g = 10
+masaOrbit = 1
+masaZiemi = 5.972 * 10**13
+stalaG = 6.67 
+promienZiemi = 6.37 * 10**6
+g = 9.81
 kroki = 100000
-deltaT = 0.01
+deltaT = 1
+masy = stalaG * masaZiemi * masaOrbit
 
-def pierwszaPredkoscKosmiczna(g, distance):
-    return np.sqrt(g * distance)
 
-x = np.linspace(0, 10000, 500)
-y = pierwszaPredkoscKosmiczna(g, x)
+def pierwszaPredkoscKosmiczna(distance):
+    ''' oblicza pierwsza prędkosć kosmiczną dla ciała na wysokosci "distance" nad poziomem morza '''
+    return np.sqrt(stalaG*masaZiemi/(distance+promienZiemi))
 
+x = np.linspace(0, 10000000, 100)
+y = pierwszaPredkoscKosmiczna(x)
+print('Pierwsza prędkosć kosmiczna: {}'.format(y[0]))
+print('Druga prędkosć kosmiczna: {}'.format(np.sqrt(2)*y[0]))
 plt.figure(0)
 plt.plot(x,y)
 plt.title('Pierwsza prędkosć kosmiczna')
 plt.ylabel('prędkosć w m/s')
-plt.xlabel('wysokosć w m')
+plt.xlabel('wysokosć w m nad ziemią')
 plt.show
 
-distance = 100
-vx = 0
-vy = np.sqrt(2) * pierwszaPredkoscKosmiczna(g, distance)
+distance = promienZiemi + 1000
+vx = 10700
+vy = 0
 
 x = np.empty((kroki,))
 y = np.empty((kroki,))
 x[0] = 0
 y[0] = distance
-ay = -g
-ax = 0 
+f = masy / distance**2
+fx = -f * x[0]/distance 
+fy = -f * y[0]/distance
+ax = fx / masaOrbit
+ay = fy / masaOrbit
 
 for moment in range(1, kroki):
     x[moment] = x[moment-1] + vx*deltaT
     y[moment] = y[moment-1] + vy*deltaT
     distance = np.sqrt(x[moment]**2 + y[moment]**2)
-    ax = -g * x[moment]/distance
-    ay = -g * y[moment]/distance
+    f = masy / distance**2
+    fx = -f * x[moment]/distance 
+    fy = -f * y[moment]/distance
+    ax = fx / masaOrbit
+    ay = fy / masaOrbit
     vx = vx + ax * deltaT
     vy = vy + ay * deltaT
 
 plt.figure(1)
 plt.plot(x,y)
+
 plt.axis('equal')
